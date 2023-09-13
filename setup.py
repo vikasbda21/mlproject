f0bf375233ac24f51d0665211c948a2e42f414c4
viku <viku9358@gmail.com>
2023-09-13 12:21:05 +0530

    Your commit message

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+HYPHEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function returns a list of requirements
+    '''
+    requirements = []
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+
+setup(
+name = 'mlproject',
+version = '0.0.1',
+author = 'Vikas',
+author_email = 'viku9358@gmail.com',
+packages = find_packages(),
+install_requires = get_requirements('requirements.txt')
+
+
+)
