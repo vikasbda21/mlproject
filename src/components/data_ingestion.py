412babe10955f395e7a3091c994563cfcae09b5f
viku <viku9358@gmail.com>
2023-10-27 17:01:10 +0530

    done the data ingestion from csv file and create artifacts env

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -55,7 +55,8 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
+    obj.initiate_data_ingestion()
+    #train_data,test_data=obj.initiate_data_ingestion()
 
     # data_transformation=DataTransformation()
     # train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
